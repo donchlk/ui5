411c31a9679d1582f5ca2561ff32c7393149476f
Alexandros Ntontsov <alexandros.ntontsov@adesso-orange.com>
2023-10-09 07:48:12 +0000

    stand 2

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,44 +1,61 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    'sap/m/MessageToast'
+    'sap/m/MessageToast',
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("gruppe3.controller.Main", {
             onInit: function () {
 
-            //    var oModel = new sap.ui.model.json.JSONModel();
-            //    oModel.loadData("data/artists2.json", {}, false);
-            //    this.getView().setModel(oModel, "artMod");
-            //    var oModel2 = new sap.ui.model.json.JSONModel();
-            //    oModel2.setData({
-            //        "movie": {},
-            //        "artist": {}
-            //    });
-            //    this.getView().setModel(oModel2, "movSng");
             },
     
-            onExpandFirstLevel: function() {
-                var oTreeTable = this.byId("ttab1");
-                oTreeTable.expandToLevel(1);
+            onSearch: function (oEvent) {
+                // add filter for search
+                var aFilters = [];
+                var sQuery = oEvent.getSource().getValue();
+                if (sQuery && sQuery.length > 0) {
+                    var filter = new Filter("Name", FilterOperator.Contains, sQuery);
+                    aFilters.push(filter);
+                }
+    
+                // update list binding
+                var oList = this.byId("idList");
+                var oBinding = oList.getBinding("items");
+                oBinding.filter(aFilters, "Application");
             },
     
-            onCollapseAll: function() {
-                var oTreeTable = this.byId("ttab1");
-                oTreeTable.collapseAll();
-            },
+            onSelectionChange: function (oEvent) {
+                var oList = oEvent.getSource();
+                var oLabel = this.byId("idFilterLabel");
+                var oInfoToolbar = this.byId("idInfoToolbar");
     
-            onExpandSelection: function() {
-                var oTreeTable = this.byId("ttab1");
-                oTreeTable.expand(oTreeTable.getSelectedIndices());
+                // With the 'getSelectedContexts' function you can access the context paths
+                // of all list items that have been selected, regardless of any current
+                // filter on the aggregation binding.
+                var aContexts = oList.getSelectedContexts(true);
+    
+                // update UI
+                var bSelected = (aContexts && aContexts.length > 0);
+                var sText = (bSelected) ? aContexts.length + " selected" : null;
+                oInfoToolbar.setVisible(bSelected);
+                oLabel.setText(sText);
             },
 
-            onListItemPress: function (oEvent) {
-                MessageToast.show("Artikel : " + oEvent.getSource().getTitle() + " wurde bestellt");
+            onSelectionChange: function(){
+                var list,
+                binding,
+                filter;
+                list = this.getView().byId("idList");
+                filter = new sap.ui.model.Filter("lastName",sap.ui.model.FilterOperator.Contains , "D");
+                binding = list.getBinding("items");
+                binding.filter(filter,"Application");
+                binding.refresh(true);
             }
             
             
